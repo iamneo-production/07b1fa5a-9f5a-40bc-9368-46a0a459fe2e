b0058ed8bd3d218769ec85001080344aa60b6f00
Yashasvi-Berbalaje <yberbalaje404@gmail.com>
2022-04-12 16:55:26 +0000

    Theme admin API's

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,36 +1,24 @@
-import React from 'react';
-import {BrowserRouter as Router,Navigate,Route,Routes} from 'react-router-dom';
-import Home from './pages/home/index';
-import { isAuthenticated } from './helper/Authentication';
-import Admin from './pages/admin/index';
-import SignIn from './pages/signin/index';
-import SignUp from './pages/signup/index';
-import NotFound from './pages/notfound/index';
-import ProtectedRoutes from './helper/ProtectedRoutes.js';
-
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-      <Router>
-          <Routes>
-                <Route
-                      exact
-                      path="/"
-                      element={ isAuthenticated() ?
-                        <Navigate to="/home" /> :
-                        <Navigate to="/signin" /> }
-                    />
-            <Route  element={<ProtectedRoutes/>}>
-            <Route path="/home" exact element={<Home/>}></Route>
-            <Route path="/admin" exact element={<Admin/>}></Route>
-            </Route>
-            <Route path="/signup" exact element={<SignUp/>}></Route>
-            <Route path="/signin" exact element={<SignIn/>}></Route>
-            <Route path="*"  element={<NotFound/>}></Route>
-          </Routes>
-        </Router>
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
